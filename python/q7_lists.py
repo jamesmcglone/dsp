88b29d5a33e6228382fde965216b8d49dc6bf059
jamesmcglone <jam@alumni.princeton.edu>
2016-09-18 09:55:27 -0700

    Update q7_lists.py

--- python/q7_lists.py
+++ python/q7_lists.py
@@ -7,7 +7,6 @@ def match_ends(words):
     Given a list of strings, return the count of the number of strings
     where the string length is 2 or more and the first and last chars
     of the string are the same.
-
     >>> match_ends(['aba', 'xyz', 'aa', 'x', 'bbb'])
     3
     >>> match_ends(['', 'x', 'xy', 'xyx', 'xx'])
@@ -15,8 +14,16 @@ def match_ends(words):
     >>> match_ends(['aaa', 'be', 'abc', 'hello'])
     1
     """
-    raise NotImplementedError
+    count = 0
+    for x in words:
+        if (len(x) >= 2) and (x[0] == x[-1]):
+            count += 1
 
+    return count
+
+#print match_ends(['aba', 'xyz', 'aa', 'x', 'bbb'])
+#print match_ends(['', 'x', 'xy', 'xyx', 'xx'])
+#print match_ends(['aaa', 'be', 'abc', 'hello'])
 
 def front_x(words):
     """
@@ -24,7 +31,6 @@ def front_x(words):
     order, except group all the strings that begin with 'x' first.
     e.g. ['mix', 'xyz', 'apple', 'xanadu', 'aardvark'] yields
          ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'].
-
     >>> front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa'])
     ['xaa', 'xzz', 'axx', 'bbb', 'ccc']
     >>> front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa'])
@@ -32,8 +38,15 @@ def front_x(words):
     >>> front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark'])
     ['xanadu', 'xyz', 'aardvark', 'apple', 'mix']
     """
-    raise NotImplementedError
 
+    x_words = filter(lambda w:  w[0] == 'x', words)
+    other_words = filter(lambda w: w[0] != 'x', words)
+ 
+    return sorted(x_words) + sorted(other_words)
+
+front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa'])
+front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa'])
+front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark'])
 
 def sort_last(tuples):
     """
@@ -41,7 +54,6 @@ def sort_last(tuples):
     increasing order by the last element in each tuple.
     e.g. [(1, 7), (1, 3), (3, 4, 5), (2, 2)] yields
          [(2, 2), (1, 3), (3, 4, 5), (1, 7)].
-
     >>> sort_last([(1, 3), (3, 2), (2, 1)])
     [(2, 1), (3, 2), (1, 3)]
     >>> sort_last([(2, 3), (1, 2), (3, 1)])
@@ -49,7 +61,12 @@ def sort_last(tuples):
     >>> sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)])
     [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
     """
-    raise NotImplementedError
+
+    return sorted(tuples, key=lambda num: num[-1])
+
+#print sort_last([(1, 3), (3, 2), (2, 1)])
+#print sort_last([(2, 3), (1, 2), (3, 1)])
+#print sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)])
 
 
 def remove_adjacent(nums):
@@ -58,7 +75,6 @@ def remove_adjacent(nums):
     elements have been reduced to a single element, so [1, 2, 2, 3]
     returns [1, 2, 3]. You may create a new list or modify the passed
     in list.
-
     >>> remove_adjacent([1, 2, 2, 3])
     [1, 2, 3]
     >>> remove_adjacent([2, 2, 3, 3, 3])
@@ -68,8 +84,13 @@ def remove_adjacent(nums):
     >>> remove_adjacent([])
     []
     """
-    raise NotImplementedError
 
+    return reduce(lambda x, y: x + [y] if x[-1] != y else x, nums[1:], nums[:1])
+
+#print remove_adjacent([1, 2, 2, 3])
+#print remove_adjacent([2, 2, 3, 3, 3])
+#print remove_adjacent([3, 2, 3, 3, 3])
+#print remove_adjacent([])
 
 def linear_merge(list1, list2):
     """
@@ -77,7 +98,6 @@ def linear_merge(list1, list2):
     merged list of all the elements in sorted order. You may modify
     the passed in lists. Ideally, the solution should work in "linear"
     time, making a single pass of both lists.
-
     >>> linear_merge(['aa', 'xx', 'zz'], ['bb', 'cc'])
     ['aa', 'bb', 'cc', 'xx', 'zz']
     >>> linear_merge(['aa', 'xx'], ['bb', 'cc', 'zz'])
@@ -85,4 +105,13 @@ def linear_merge(list1, list2):
     >>> linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb'])
     ['aa', 'aa', 'aa', 'bb', 'bb']
     """
-    raise NotImplementedError
+
+    list1.extend(list2)
+    return sorted(list1)
+
+
+print linear_merge(['aa', 'xx', 'zz'], ['bb', 'cc'])
+print linear_merge(['aa', 'xx'], ['bb', 'cc', 'zz'])
+print linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb'])
+
+
